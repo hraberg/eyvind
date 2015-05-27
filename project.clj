1e7693bc9ad0063245e49cc90a002091a544b396
Håkan Råberg <hraberg@yahoo.se>
2015-05-27 01:51:43 +0100

    Let's use msgpack for now.

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0-RC1"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [com.taoensso/nippy "2.9.0-RC2"]
+                 [clojure-msgpack "1.1.0"]
                  [org.zeromq/jeromq "0.3.4"]
                  [org.zeromq/cljzmq "0.1.4" :exclusions [org.zeromq/jzmq]]]
   :java-source-paths ["src"]
