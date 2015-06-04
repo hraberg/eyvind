9862a9dbc98157d7fa1fc07b5c0cf4662d61cd3e
Håkan Råberg <hraberg@yahoo.se>
2015-06-04 08:58:07 +0100

    Will need at least one loging facade

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0-RC1"]
+                 [org.clojure/tools.logging "0.3.1"]
                  [org.zeromq/jeromq "0.3.4"]
                  [org.zeromq/cljzmq "0.1.4" :exclusions [org.zeromq/jzmq]]]
   :profiles {:dev {:dependencies [[org.clojure/core.async "0.1.346.0-17112a-alpha"]
