849fd8614e7c953568902c245204967f0efd0aeb
Håkan Råberg <hraberg@yahoo.se>
2015-07-24 10:42:10 +0100

    Updating deps

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "http://hraberg.github.io/eyvind/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0-alpha2"]
                  [org.clojure/tools.logging "0.3.1"]
-                 [org.zeromq/jeromq "0.3.4"]
+                 [org.zeromq/jeromq "0.3.5"]
                  [org.zeromq/cljzmq "0.1.4" :exclusions [org.zeromq/jzmq]]]
   :profiles {:dev {:dependencies [[org.clojure/core.async "0.1.346.0-17112a-alpha"]
                                   [com.taoensso/nippy "2.9.0"]]}}
