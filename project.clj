d37402eb0df8f13f77b86e5b22cf622f734f4e5e
Håkan Råberg <hraberg@yahoo.se>
2016-08-19 22:32:25 +0100

    Clojure 1.9.0-alpha11

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://hraberg.github.io/eyvind/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha10"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha11"]
                  [org.clojure/tools.logging "0.3.1"]
                  [org.zeromq/jeromq "0.3.5"]
                  [org.zeromq/cljzmq "0.1.4" :exclusions [org.zeromq/jzmq]]]
